33304247da7bd67dc3e28b3515104fafaf10c204
ysonggit <ysonggit@users.noreply.github.com>
2016-11-25 19:41:24 -0800

    Update HelloWorld.scala

--- New Project-20161126/HelloWorld.scala
+++ New Project-20161126/HelloWorld.scala
@@ -2,8 +2,13 @@ import scala.io.Source
 
 object HelloWorld {
    def main(args: Array[String]) {
-      for(line <- Source.fromFile("myfile.txt").getLines())
-        println(line)
+        val dbName = "ams_test"
+        for(line <- Source.fromFile("myfile.txt").getLines()){
+            if (line.contains("ams")){
+                val line2 = line.replace("ams", dbName)
+                println(line2)
+            }
+        }
    }
 }
 
